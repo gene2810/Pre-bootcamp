f467f93a240ef76dc8ce6b4cfdd23bdadde9aab1
gene2810 <genesis2810programacion.iccp@gmail.com>
2022-08-19 12:15:54 -0400

    subiendo prrediccion

---
+++ Prediccion_de_fragmento_de_codigo2.js
@@ -0,0 +1,28 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+// ["Paula","Smith","Calle Principal 1234", "St. Louis","MO", 12345]; 
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+//produce= ["manzanas, "naranjas"];
+//frozen= ["brócoli","helado"];
+//frozen.push ("croquetas de papa");
+//console.log(frozen);
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+//movielibrary=["bambi","E.T","Toy Story"];
+//movielibrary.push("Zoro");
+//movieLibrary[1]= "Beatlejuice";
+//console.log(movielibrary);
+ 
+
+    
